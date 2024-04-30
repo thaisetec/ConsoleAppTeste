758509267deff93f6975fed749a173e1017a9a42
Usuario <Usuario@CPD03-M01>
2024-04-30 10:45:43 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppTeste/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTeste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nome = { "joão", "pedro", "maria" };
+            Console.WriteLine("digite um numero de 0 a 2;");
+            int i =int.Parse(Console.ReadLine());
+            Console.WriteLine("o nome selecionado foi:" + nome[i]);
+            Console.ReadKey();
+        }
+    }
+}
